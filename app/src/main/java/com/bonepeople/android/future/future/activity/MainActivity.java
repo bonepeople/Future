3c16af0fb08e89e87a7ea27a428d483c59377d38
bonepeople <bonepeople@sina.com>
2018-01-20 23:45:47 +0800

    添加对列表页面的调用

--- app/src/main/java/com/bonepeople/android/future/future/activity/MainActivity.java
+++ app/src/main/java/com/bonepeople/android/future/future/activity/MainActivity.java
@@ -3,16 +3,30 @@ package com.bonepeople.android.future.future.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.bonepeople.android.future.future.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        startActivity(new Intent(getApplicationContext(), ADActivity.class));
+
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(getApplicationContext(), ADActivity.class));
+        Button _button_list = findViewById(R.id.button_list);
+        _button_list.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_list:
+                startActivity(new Intent(getApplicationContext(), ListActivity.class));
+                break;
+        }
     }
 }
