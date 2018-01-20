3b29c3fb9c8437a073a8bf18b1597129a056a4ae
bonepeople <bonepeople@sina.com>
2018-01-20 20:31:55 +0800

    使用shimmer-android开源框架，实现提示文字闪烁的效果
    源码：https://github.com/facebook/shimmer-android
    主页：http://facebook.github.io/shimmer-android/
    文档：http://facebook.github.io/shimmer-android/javadoc/index.html

--- app/src/main/java/com/bonepeople/android/future/future/activity/ADActivity.java
+++ app/src/main/java/com/bonepeople/android/future/future/activity/ADActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.bonepeople.android.future.future.R;
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.r0adkll.slidr.Slidr;
 
 public class ADActivity extends AppCompatActivity {
@@ -13,5 +14,14 @@ public class ADActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ad);
         Slidr.attach(this);
+
+        ShimmerFrameLayout _layout = findViewById(R.id.shimmer);
+        _layout.setDuration(2000);
+        _layout.startShimmerAnimation();
+    }
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
