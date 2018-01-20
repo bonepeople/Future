23099183cfcf73936b184b58577a74f78029f83c
bonepeople <bonepeople@sina.com>
2018-01-20 18:56:46 +0800

    使用Slidr开源框架，实现侧滑关闭的效果
    https://github.com/r0adkll/Slidr

--- app/src/main/java/com/bonepeople/android/future/future/activity/ADActivity.java
+++ app/src/main/java/com/bonepeople/android/future/future/activity/ADActivity.java
@@ -2,25 +2,16 @@ package com.bonepeople.android.future.future.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 import com.bonepeople.android.future.future.R;
+import com.r0adkll.slidr.Slidr;
 
 public class ADActivity extends AppCompatActivity {
-    ImageView _image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ad);
-
-        _image = findViewById(R.id.image_ad);
-        _image.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        Slidr.attach(this);
     }
 }
