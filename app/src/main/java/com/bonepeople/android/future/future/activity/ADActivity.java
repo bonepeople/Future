a2e0fb3ba47674208a981d295214038f74657fff
bonepeople <bonepeople@sina.com>
2019-02-09 14:38:49 +0800

    在广告页添加倒计时功能
    使用CountDownTimer进行计时
    更新com.facebook.shimmer:shimmer，解决刷新界面时动画会重新开始的bug

--- app/src/main/java/com/bonepeople/android/future/future/activity/ADActivity.java
+++ app/src/main/java/com/bonepeople/android/future/future/activity/ADActivity.java
@@ -1,13 +1,17 @@
 package com.bonepeople.android.future.future.activity;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.bonepeople.android.future.future.R;
-import com.facebook.shimmer.ShimmerFrameLayout;
 import com.r0adkll.slidr.Slidr;
 
 public class ADActivity extends AppCompatActivity {
+    private TextView textView_timer;
+    CountDownTimer timer;
+    private int tickCount = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +19,27 @@ public class ADActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ad);
         Slidr.attach(this);
 
-        ShimmerFrameLayout _layout = findViewById(R.id.shimmer);
-        _layout.setDuration(2000);
-        _layout.startShimmerAnimation();
+        textView_timer = findViewById(R.id.textView_timer);
+
+        timer = new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                textView_timer.setText(getString(R.string.caption_text_timer, tickCount));
+                tickCount--;
+            }
+
+            @Override
+            public void onFinish() {
+                finish();
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        timer.cancel();
+        super.onDestroy();
     }
 
     @Override
